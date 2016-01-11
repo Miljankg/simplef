2de767125840c9e19f58c507ecfc4702fa43d17e
Miljan Pantic <panticmiljan.kg@gmail.com>
2016-01-11 05:13:04 +0100

    404 reworked

--- src/framework/config/system/config_lang.php
+++ src/framework/config/system/config_lang.php
@@ -4,7 +4,7 @@ $config['is_site_multilang']    = true;
 
 /* Languages */
 $config['default_language']     = 'en';
-$config['avaliable_langs']      = array('en');
+$config['available_langs']      = array('en');
 $config['lang_dir']             = $config['app_dir'] . 'lang' . $config['ds'];
 
 
