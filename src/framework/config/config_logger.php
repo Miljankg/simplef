e05315afcb3d0789a1cca74c4446e9b96c56a5a1
Miljan Pantic <panticmiljan.kg@gmail.com>
2016-04-11 00:16:20 +0200

    Fixed log file path

--- src/framework/config/config_logger.php
+++ src/framework/config/config_logger.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['error_log_enabled']             = true;
-$config['log_file']                      = '#$framework_dir#log\\sf.log';
+$config['log_file']                      = '#$framework_dir#log/sf.log';
 $config['log_time_format']               = '[ Y-m-d H:i:s ]';
 $config['new_line']                      = '
 ';
